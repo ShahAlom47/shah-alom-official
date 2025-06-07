1ec981f7d494105eea1c490a41e3256b988bc450
ShahAlom47 <sahalom4729@gmail.com>
2025-06-07 11:27:20 +0600

    update folder stucture

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Admin ||Shah Alom Official",
+  description: "Shah Alom's official website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        {children}
+      </body>
+    </html>
+  );
+}
