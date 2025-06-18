46e50271a05ca7cd97be01ca2c456ebb5dce7b61
ShahAlom47 <sahalom4729@gmail.com>
2025-06-19 00:32:18 +0600

    update  loading page  and style

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import React from "react";
-import "../style/loadingSpinner.css"; // CSS টা নিচে দেওয়া হচ্ছে
+import "../style/loadingSpinner.css";
 
 const Loading = () => {
   return (
-    <div className="min-h-screen bg-black flex flex-col justify-center items-center text-white relative overflow-hidden">
-      <div className="custom-loader"></div>
-      <p className="mt-6 text-xl font-semibold tracking-wide">Loading...</p>
-      <div className="glow-line "></div>
+    <div className="min-h-screen bg-black flex flex-col justify-center items-center text-white">
+      <div className=" w-40 h-40  relative flex items-center justify-center">
+        <div className="custom-loader"></div>
+        <div className="custom-loader2"></div>
+        <p className="text-sm font-semibold tracking-wide ">Loading...</p>
+      </div>
+
+      <div className="glow-line"></div>
     </div>
   );
 };
