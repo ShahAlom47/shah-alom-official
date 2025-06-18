ead0613d7668f7273e0642559f5f3cae60019be0
ShahAlom47 <sahalom4729@gmail.com>
2025-06-18 23:34:24 +0600

    update loading  componet

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-     Loading...
+    <div className="flex justify-center items-center min-h-screen bg-blackDeep text-white">
+      <div className="flex flex-col items-center space-y-4">
+        {/* Spinner */}
+        <FaSpinner className="animate-spin text-4xl text-primary" />
+
+        {/* Text */}
+        <p className="text-lg font-medium">Loading, please wait...</p>
+      </div>
     </div>
   );
 }
